0fb0f136b8eac7a4944ae7fde69d8e3ed6f2f11d
Deepak Verma <dpakverma789@gmail.com>
2024-05-01 14:05:05 +0530

    minor fix

--- django_web_app_module/settings.py
+++ django_web_app_module/settings.py
@@ -82,7 +82,10 @@ DATABASES = {
         'USER': 'uawhyquc',
         'PASSWORD': 'FC-0MSOEBIIDaCXfXXG0GfubkoPl7UrU',
         'HOST': 'snuffleupagus.db.elephantsql.com',
-        'PORT': 5432
+        'PORT': 5432,
+        'OPTIONS': {
+           "init_command": "SET GLOBAL max_connections = 100000"
+        }
     }
 }
 
