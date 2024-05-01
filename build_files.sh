0addb5fc3f490f4cc36ce1e4292bcc30374432fd
Deepak Verma <dpakverma789@gmail.com>
2024-05-01 10:26:34 +0530

    minor fix

--- build_files.sh
+++ build_files.sh
@@ -3,10 +3,10 @@ pip3 install -r requirements.txt
 echo "======> REQUIREMENTS INSTALLED <======"
 
 echo "======> COLLECTING STATIC FILES <======"
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 echo "======> STATIC FILES COLLECTED <======"
 
 echo "======> MAKE-MIGRATIONS <======"
 python3 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3 manage.py migrate --noinput
 echo "======> MAKE-MIGRATIONS-END <======"
